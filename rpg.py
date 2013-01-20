5760cc595c396ae95d87e12178bf217635c4dede
Iury <satriani-16@hotmail.com>
2013-01-20 15:11:39 -0200

    Update rpg.py
    - corrigindo erro na movimentacao do personagem

--- rpg.py
+++ rpg.py
@@ -1,59 +1,74 @@
 # coding: utf-8
 
+
 from config import *
 from Classes import *
 from Cores import *
+from cache import *
+from Vocab import *
+import sys
+import pygame
+
 
 FPS = 16
 
 filaF, filaC, filaE, filaD = 0, 0, 0, 0
 
 # mover personagem para a esquerda
+
+
 def MPE():
     global filaE
     if teclas[K_LEFT] and not teclas[K_DOWN] and not teclas[K_UP]:
-        filaE += 1
-        if filaE == 2:
-            filaE = 0
         personagem.image = pygame.image.load(listaImagensLadoEsquerdo[filaE])
         personagem.converterImagem()
         personagem.mover(-10, 0)
         personagem.px -= 1
+        filaE += 1
+        if filaE > 2:
+            filaE = 0
+    print(filaE)
 #mover personagem para a direita
+
+
 def MPD():
     global filaD
     if teclas[K_RIGHT] and not teclas[K_DOWN] and not teclas[K_UP]:
-        filaD += 1
-        if filaD == 2:
-            filaD = 0
         personagem.image = pygame.image.load(listaImagensLadoDireito[filaD])
         personagem.converterImagem()
         personagem.mover(10, 0)
         personagem.px += 1
+        filaD += 1
+        if filaD > 2:
+            filaD = 0
 
 # mover personagem para cima
+
+
 def MPC():
     global filaC
     if teclas[K_UP]:
-        filaC += 1
-        if filaC == 2:
-            filaC = 0
         personagem.image = pygame.image.load(listaImagensCostas[filaC])
         personagem.converterImagem()
         personagem.mover(0, -10)
         personagem.py -= 1
+        filaC += 1
+        if filaC > 2:
+            filaC = 0
 
 # mover personagem para baixo
+
+
 def MPB():
     global filaF
     if teclas[K_DOWN]:
-        filaF += 1
-        if filaF == 2:
-            filaF = 0
         personagem.image = pygame.image.load(listaImagensFrente[filaF])
         personagem.converterImagem()
         personagem.mover(0, 10)
         personagem.py += 1
+        filaF += 1
+        if filaF > 2:
+            filaF = 0
 
 #=======================
 
@@ -87,9 +102,18 @@ for b in range(-4, 76):
     a += 1
 l1 = [-10]
 l2 = [6]
-iniciarConversa = [43, 0]
+
+#parede esquerda
+
+parede = [-2, []]
+for a in range(-10, 7):
+    parede[1].append(a)
+
 #===================================
-teclas = {K_LEFT: False, K_RIGHT: False, K_UP: False, K_DOWN: False, K_RETURN: False, 27: False}  # obs 27 = tecla 'esc'
+iniciarConversa = [43, 0]
+
+teclas = {K_LEFT: False, K_RIGHT: False, K_UP: False, K_DOWN: False,
+          K_RETURN: False, 27: False}  # obs 27 = tecla 'esc'
 musica.play()
 fundo = fundo.convert()
 pygame.display.flip()
@@ -100,10 +124,15 @@ while True:
         valor = (evento.type == KEYDOWN)
         if evento.key in teclas.keys():
             teclas[evento.key] = valor
+
     if teclas[27]:
-        pygame.exit()
+        #pygame.exit()
         sys.exit()
-    if personagem.px in lx:
+    if personagem.px == parede[0] and personagem.py in parede[1]:
+        MPD()
+        MPB()
+        MPC()
+    elif personagem.px in lx:
         if personagem.py in l1:
             MPB()
             MPD()
@@ -123,7 +152,7 @@ while True:
             tela.blit(frase.frases, (200, 500))
             pygame.display.flip()
 
-    #print(personagem.px,personagem.py)
+    #print(personagem.px, personagem.py)
 
     grupo.clear(tela, fundo)
     pygame.display.update(grupo.draw(tela))
